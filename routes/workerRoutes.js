b9f12770de43600b2fb44121ddb0ddd828ee07ac
nasarali03 <nasarali1805@gmail.com>
2025-03-27 17:51:05 +0500

    remove code

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -8,11 +8,5 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/apply", authMiddleware, applyAsWorker); // Worker applies for approval
-router.post(
-  "/upload-video",
-
-  authMiddleware,
-  uploadWorkerVideo
-); // Worker uploads video
 
 module.exports = router;
