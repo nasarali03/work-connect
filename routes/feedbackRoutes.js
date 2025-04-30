f095c7c5d7f58807a7d6bed4687628f187974fa8
nasarali03 <nasarali1805@gmail.com>
2025-04-30 18:08:09 +0500

    Implement feedback controller with CRUD operations and update feedback routes

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
+
 const {
   createFeedback,
   getAllFeedbacks,
@@ -13,3 +14,5 @@ router.post("/add-feedback", authMiddleware, createFeedback);
 router.get("/all-feedbacks", authMiddleware, getAllFeedbacks);
 router.get("/feedback/:id", authMiddleware, getFeedbackById);
 router.delete("/delete-feedback/:id", authMiddleware, deleteFeedback);
+
+module.exports = router;
